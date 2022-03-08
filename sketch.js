d6c035ffa2c0537b705e5cef0bb968a9e68f7730
Ammelic <87152962+Ammelic@users.noreply.github.com>
2022-03-08 17:38:11 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var trex, trex_running;
+var edges;
+function preload(){
+  trex_running=loadAnimation("trex1.png","trex3.png","trex4.png");
+
+}
+
+function setup(){
+createCanvas(600,200);
+trex=createSprite(50,170,30,70);
+trex.addAnimation("running",trex_running);
+trex.scale=0.7;
+edges=createEdgeSprites();
+
+ 
+}
+
+function draw(){
+  background("white");
+
+  if(keyDown("space")){
+  trex.velocityY+=-10;
+  }
+ trex.velocityY+=0.5;
+ trex.collide(edges[3])
+
+  drawSprites();
+  
+
+}
